ebd5d1e6c0aaca58f2bade003e8c1838ba4f0e30
yanpeng <peng.yan@chargedot.com>
2016-10-13 10:23:42 +0800

    java io test

---
+++ src/main/java/com/yp/netty_demos/io/socket/TimerServer.java
@@ -0,0 +1,45 @@
+package com.yp.netty_demos.io.socket;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TimerServer {
+    public static void main(String[] args) {
+        int port=8080;
+        if(args!=null&&args.length>0){
+            try{
+                port=Integer.parseInt(args[0]);
+            }catch(Exception e){
+            }
+        }
+        
+        ServerSocket server=null;
+        try{
+            server=new ServerSocket(port);
+            System.out.println("The time server is start in port："+port);
+            Socket socket=null;
+            while(true){
+                socket=server.accept();
+                if(socket!=null)
+                    new Thread(new TimeServerHandler(socket)).start();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            if(server!=null){
+                System.out.println("The time server close");
+                try {
+                    server.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }finally{
+                    server=null;
+                }
+            }
+        }
+        
+        
+    }
+}
---
+++ src/main/java/com/yp/netty_demos/io/sockethandlepool/TimeServerHandler.java
@@ -0,0 +1,48 @@
+package com.yp.netty_demos.io.sockethandlepool;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Date;
+
+public class TimeServerHandler implements Runnable {
+    private Socket socket;
+    
+    public TimeServerHandler(Socket socket){
+        this.socket=socket;
+    }
+
+    @Override
+    public void run() {
+        try(BufferedReader in=new BufferedReader(new InputStreamReader(socket.getInputStream()))){
+            try(PrintWriter out=new PrintWriter(this.socket.getOutputStream(),true)){
+                String time=null;
+                String body=null;
+                while(true){
+                    body=in.readLine();
+                    if(body==null){
+                        break;
+                    }
+                    System.out.println("The time server receive the order:"+body);
+                    time="query time".equals(body)?new Date().toString():"bad order";
+                    out.println(time);
+                }
+            }
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+            if(this.socket!=null){
+                try {
+                    this.socket.close();
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+}
